ef3ffdf023442143188be3b1356d23f2700032a5
Guilherme de Freitas Anacleto <48931728+Paquista0@users.noreply.github.com>
2022-03-29 11:56:33 -0300

    update 29-03-2022 11:56

--- server.js
+++ server.js
@@ -1,21 +1,10 @@
 // Instalar nodemon - para ter refresh automatico ao salvar no arquivo do server
 // npm install nodemon@2.0.15 -D
 
-const http = require("http"); //importar metodo interno HTTP
-const port= 3000;       //porta server
+import app from './src/app.js'
 
-const rotas = {
-    '/':'Curso de Node',
-    '/livros' : 'Entrei na pagina de livros',
-    '/autores' : 'Listagem de autores',
-    '/editora': 'Pag editora',
-    '/sobre' : 'Info sobre o projeto'
-}
-
-const server = http.createServer((req, res) =>{         //criar server e oque ele vai fazer: req e res (requisição e resposta) e na arrow function o callback
-    res.writeHead(200, {'Content-Type':'text/plain'});       //resposta -> cabeçalho, status = 200 = OK , tipo = texto/plano -> callback
-    res.end(rotas[req.url]);        //end -> oque vai ser exibido no final -> callback
-})      
-server.listen(port, ()=> { //server listen = escutando porta, retornando mensagemd e sucesso
+const port = process.env.PORT || 3000; //produçãou || homologação -> portas server
+ 
+app.listen(port, ()=> { //server listen = escutando porta, retornando mensagemd e sucesso
     console.log(`Servidor escutando em http://localhost:${port}`); 
 })
--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import express from "express"; 
 
-const app = express()
+
+const app = express();
+
+app.use(express.json());
+
 
 const livros = [
     {id: 1, "titulo": "Senhor dos Aneis"},
@@ -8,7 +12,18 @@ const livros = [
 ]
 
 app.get('/',(req, res) => {
-    res.statusd(200).send('Curso de Node');
+    res.status(200).send('Curso de Node');
          
 }) 
- //time aula  7:00min  
+
+app.get('/livros',(req, res) =>{
+    res.status(200).json(livros);    
+})
+
+app.post('/livros',(req, res) =>{
+    livros.push(req.body);
+    res.status(201).send('Livro foi cadastrado com sucesso')
+})
+
+export default app
+
