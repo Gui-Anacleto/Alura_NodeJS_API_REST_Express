805bd420a2a2dbc0dce32d7eb3fbbaa5e1e025db
Guilherme de Freitas Anacleto <48931728+Paquista0@users.noreply.github.com>
2022-03-29 18:16:19 -0300

    29-03-2022 18:16

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express"; 
 
 
+
 const app = express();
 
 app.use(express.json());
@@ -16,14 +17,30 @@ app.get('/',(req, res) => {
          
 }) 
 
+//visualiza
 app.get('/livros',(req, res) =>{
     res.status(200).json(livros);    
 })
 
+app.get('/livros/:id', (req, res) =>{
+    let index = buscaLivro(req.params.id);
+    res.json(livros[index])
+})
+
+//adicionar
 app.post('/livros',(req, res) =>{
     livros.push(req.body);
     res.status(201).send('Livro foi cadastrado com sucesso')
 })
+//alterar
+app.put('/livros/:id', (req, res) =>{
+    let index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.json(livros)
+})
 
+function buscaLivro(id){
+    return livros.findIndex(livro => livro.id == id);
+}
 export default app
 
