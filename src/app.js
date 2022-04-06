22bbe89b81dfa5f3f2eb1fb956135ad4add6313c
Guilherme de Freitas Anacleto <48931728+Paquista0@users.noreply.github.com>
2022-04-06 18:02:45 -0300

    06/04/2022 - 18:02

--- src/app.js
+++ src/app.js
@@ -1,27 +1,35 @@
 import express from "express"; 
+import db from "./config/dbConnect.js";
+import livros from "./models/Livro.js";
 
-
+db.on("error", console.log.bind(console, 'ERROR DE CONEXAO !'))
+db.once("open", () =>{
+    console.log('conexão com o banco feita com sucesso')
+})
 
 const app = express();
 
 app.use(express.json());
 
 
-const livros = [
+/* const livros = [
     {id: 1, "titulo": "Senhor dos Aneis"},
     {id: 2, "titulo": "O Hobbit"}
-]
+] */
 
 app.get('/',(req, res) => {
     res.status(200).send('Curso de Node');
          
 }) 
 
-//visualiza
+//visualiza todos os livros
 app.get('/livros',(req, res) =>{
-    res.status(200).json(livros);    
+    livros.find((err, livros) => {
+        res.status(200).json(livros);   
+    })
 })
 
+//visualiza livro por ID
 app.get('/livros/:id', (req, res) =>{
     let index = buscaLivro(req.params.id);
     res.json(livros[index])
@@ -39,6 +47,16 @@ app.put('/livros/:id', (req, res) =>{
     res.json(livros)
 })
 
+//deletar
+app.delete('/livros/:id', (req, res) =>{
+    let{id} = req.params;
+    let {index} = buscaLivro(id);
+    livros.splice(index, 1); 
+    res.send(`Livro ${id} removido com successo`)
+})
+
+
+//função buscar livro
 function buscaLivro(id){
     return livros.findIndex(livro => livro.id == id);
 }
