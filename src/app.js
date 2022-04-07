fcd04e68b2c3db0ea509cb1d8d63436a3667f7f3
Guilherme de Freitas Anacleto <48931728+Paquista0@users.noreply.github.com>
2022-04-07 11:59:54 -0300

    07/04/2022  -  11:59

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express"; 
 import db from "./config/dbConnect.js";
 import livros from "./models/Livro.js";
+import routes from "./routes/index.js";
+
 
 db.on("error", console.log.bind(console, 'ERROR DE CONEXAO !'))
 db.once("open", () =>{
@@ -11,23 +13,24 @@ const app = express();
 
 app.use(express.json());
 
+routes(app);
 
 /* const livros = [
     {id: 1, "titulo": "Senhor dos Aneis"},
     {id: 2, "titulo": "O Hobbit"}
 ] */
 
-app.get('/',(req, res) => {
+/* app.get('/',(req, res) => {
     res.status(200).send('Curso de Node');
          
-}) 
+})  */
 
 //visualiza todos os livros
-app.get('/livros',(req, res) =>{
+/* app.get('/livros',(req, res) =>{
     livros.find((err, livros) => {
         res.status(200).json(livros);   
     })
-})
+}) */
 
 //visualiza livro por ID
 app.get('/livros/:id', (req, res) =>{
--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+//local = mongoose.connect("mongodb://127.0.0.1:27017/alura-node");
 mongoose.connect("mongodb+srv://alura:123@alura.ghqga.mongodb.net/alura-node")
 
 let db = mongoose.connection;
